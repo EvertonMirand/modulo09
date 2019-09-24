b53c9058e66215e2a12741620e1250b68c669aac
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-09-23 21:04:20 -0300

    Rotas privadas e Layouts por página

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import { Dashboard, Profile, SignIn, SignUp } from '../pages';
+import Route from './Route';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
