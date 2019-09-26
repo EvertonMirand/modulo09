b5cb92954e42fc9cdae9619002fd02cc118c1801
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-09-26 16:45:07 -0300

    Requisições autenticadas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -20,7 +20,7 @@ export function* signIn({ payload }) {
       return;
     }
 
-    // api.defaults.headers.Authorization = `Bearer ${token}`;
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
 
@@ -48,7 +48,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE, setToken'),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
